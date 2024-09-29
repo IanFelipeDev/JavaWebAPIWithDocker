04d2cc15fb6669bf1d0495c581d695123b8e7011
IanFelipeDev <ianfelipelosil@gmail.com>
2024-09-29 02:01:37 -0300

    first commit

---
+++ src/main/java/com/IanFelipeDev/rest_with_docker/RestWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.IanFelipeDev.rest_with_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IanFelipeDev/rest_with_docker/RestWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IanFelipeDev.rest_with_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
